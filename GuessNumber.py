43b05892c84d8b8ae87c0048a847a6ef29421f2f
Tanishk Gautam <tanishkgautam19@gmail.com>
2026-01-27 20:20:56 +0530

    Add karna sikha kese bhi file ko

---
+++ GuessNumber.py
@@ -0,0 +1,15 @@
+print("**|Number Guessing Game|**")
+print("Guess a number between 1 and 100") 
+import random
+number_to_guess = random.randint(1,100)
+attempts = 0
+while True:
+    user_guess = int(input("Enter your guess: "))
+    attempts += 1 
+    if user_guess < number_to_guess:
+        print("Too low! Try again.")
+    elif user_guess > number_to_guess:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts.")
+        break
